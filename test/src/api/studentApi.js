4c9c6e8d6b8fa11097b8e5b13341d91735605ccb
nayanlandge <landgenayan@gmail.com>
2025-04-24 12:30:58 +0530

    Update studentApi.js

--- test/src/api/studentApi.js
+++ test/src/api/studentApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:3000/students/',
+  baseURL: 'https://new-api-peach-beta.vercel.app/students/',
 });
 
 export const getAllStudents = () => API.get('/');
